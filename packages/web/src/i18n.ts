9a433bc0043c8d8ef1c7949d629d27a21915f6d0
xuanzhi33 <xuanzhi33@qq.com>
2026-01-02 01:31:11 +0000

    feat: init i18n

---
+++ packages/web/src/i18n.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n'
+import zh from '../../../i18n/locales/zh.json'
+import en from '../../../i18n/locales/en.json'
+export default createI18n({
+  locale: 'en',
+  messages: {
+    en: {
+      message: en,
+    },
+    zh: {
+      message: zh,
+    },
+  },
+})
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,8 +3,6 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import i18n from './i18n'
 
-createApp(App)
-  .use(router)
-  .use(createPinia())
-  .mount('#app')
+createApp(App).use(router).use(createPinia()).use(i18n).mount('#app')
