d62fcbf69ad1fe291b182177731a0173ad4611aa
Acbox <acbox0328@gmail.com>
2026-01-03 19:28:07 +0800

    fix: add https prefix to cors config

--- packages/client/src/middlewares/cors.ts
+++ packages/client/src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import { cors } from '@elysiajs/cors'
 
 export const corsMiddleware = cors({
-  origin: ['chattutor.app', '*.chattutor.app', 'http://localhost:8001'],
+  origin: ['https://chattutor.app', 'https://*.chattutor.app', 'http://localhost:8001'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
