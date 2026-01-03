4cce4f4951c24173e4cea42dca98e21c18b3af52
Acbox <acbox0328@gmail.com>
2026-01-03 19:20:40 +0800

    feat: add cors config

--- packages/client/src/middlewares/cors.ts
+++ packages/client/src/middlewares/cors.ts
@@ -1,3 +1,8 @@
 import { cors } from '@elysiajs/cors'
 
-export const corsMiddleware = cors()
+export const corsMiddleware = cors({
+  origin: ['chattutor.app', '*.chattutor.app', 'http://localhost:8001'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+})
